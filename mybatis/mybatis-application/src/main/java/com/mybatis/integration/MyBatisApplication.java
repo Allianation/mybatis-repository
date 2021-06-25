9001e1d0e6199171594f28c017a59e6bd13f361b
Sergio <salliana@technisys.com>
2021-06-25 13:30:17 -0300

    Default commit

---
+++ mybatis/mybatis-application/src/main/java/com/mybatis/integration/MyBatisApplication.java
@@ -0,0 +1,15 @@
+package com.mybatis.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MyBatisApplication.class, args);
+	}
+	
+	
+
+}
